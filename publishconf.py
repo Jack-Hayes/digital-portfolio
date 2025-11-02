9447bd236e631de453b32240a632bea22a7fe0b4
Jack-Hayes <jehayes@uw.edu>
2025-11-02 12:58:28 +0100

    CI: build using publishconf.py for correct SITEURL/paths

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://Jack-Hayes.github.io/digital-portfolio"
+SITEURL = "https://jack-hayes.github.io/digital-portfolio"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
